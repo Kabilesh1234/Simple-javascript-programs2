113790bf05914ffd76b93124173f05850421b95b
Kabilesh <112680742+Kabilesh1234@users.noreply.github.com>
2024-07-31 16:11:32 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+let fruits = ['apple', 'banana', 'cherry'];
+let numbers = new Array(10);
+let colors = new Array('red', 'green', 'blue');
+console.log(fruits);
+numbers=[1,2,3,4,5,6,7,8,9,0];
+console.log(numbers);
+console.log(fruits);
